79db2eeac3a374ec808a8322224c2b7c92d07f00
rhytham-pixer <rhythamp.404@gmail.com>
2022-07-13 15:59:22 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import Default from "./Pages/Default";
-import './index.css';
-import { Header } from "./Components/Container/Header";
-function App() {
-  return (
-    <div className="App">
-      <Header>
-        
-      </Header>
-      <Default />
-    </div>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import About from './pages/About';
+import User from './components/users/User';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar />
+						<div className='container'>
+							<Alert />
+							<Routes>
+								<Route exact path='/' element={<Home />} />
+								<Route exact path='/about' element={<About />} />
+								<Route exact path='/user/:id' element={<User />} />
+								<Route element={<NotFound />} />
+							</Routes>
+						</div>
+					</div>
+				</Router>
+			</AlertState>
+		</GithubState>
+	);
+};
 
 export default App;
---
+++ src/context/github/GithubState.js
@@ -0,0 +1,81 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import * as Types from '../types';
+
+const GithubState = (props) => {
+	const initialState = {
+		users: [],
+		user: {},
+		repos: [],
+		loading: false,
+	};
+
+	const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+	// Search Users
+	const searchUsers = async (text) => {
+		setLoading();
+
+		const url = `https://api.github.com/search/users?q=${text}`;
+
+		const res = await axios.get(url);
+
+		dispatch({
+			type: Types.SEARCH_USERS,
+			payload: res.data.items,
+		});
+	};
+
+	// Get User
+	const getUser = async (username) => {
+		setLoading();
+		const url = `https://api.github.com/users/${username}`;
+
+		const res = await axios.get(url);
+
+		dispatch({
+			type: Types.GET_USER,
+			payload: res.data,
+		});
+	};
+
+	// Get Repos
+	const getUserRepos = async (username) => {
+		setLoading();
+
+		const url = `https://api.github.com/users/${username}/repos`;
+
+		const res = await axios.get(url);
+
+		dispatch({
+			type: Types.GET_REPOS,
+			payload: res.data,
+		});
+	};
+
+	// Clear Users
+	const clearUsers = () => dispatch({ type: Types.CLEAR_USERS });
+	// Set Loading
+	const setLoading = () => dispatch({ type: Types.SET_LOADING });
+
+	return (
+		<GithubContext.Provider
+			value={{
+				users: state.users,
+				user: state.user,
+				repos: state.repos,
+				loading: state.loading,
+				searchUsers,
+				clearUsers,
+				getUser,
+				getUserRepos,
+			}}
+		>
+			{props.children}
+		</GithubContext.Provider>
+	);
+};
+
+export default GithubState;
